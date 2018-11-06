a5eb58c2e19dfb1ab2ea3dee794c16622f41c65e
Neha Bhangale <bhangale.n@husky.neu.edu>
2018-11-06 01:27:43 -0500

    Initial setup for reactwithredux

---
+++ ReactJSClient/findduplicatesui/src/index.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import {createStore} from 'redux';
+import {Provider} from 'react-redux';
+import rootReducer from './Reducers';
+
+import './index.css';
+import App from './Components/App';
+
+const store = createStore(rootReducer);
+
+ReactDOM.render(
+    <Provider store={store}>
+        <App />
+    </Provider>, 
+    document.getElementById('root')
+);
+
